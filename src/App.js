3d205b3be889a66a0af915bdfc9b65d73571b2b2
Dustin Stringer <dustin.stringer@spaceforce.mil>
2022-03-07 16:19:40 -0500

    added header and moved logic to App

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import { Box, Typography } from "@mui/material";
 import { ThemeProvider } from "@mui/material/styles";
-import * as React from "react";
+import { useState } from "react";
 import "./App.css";
 import ChecklistContainer from "./ChecklistContainer";
 import Heading from "./Heading";
 import { theme } from "./Theme";
+import inprocessingItems from "./baseInprocessing.json";
 
 function App() {
-  const [header, setHeader] = React.useState({});
+  const [header, setHeader] = useState({});
+  const [items, setItems] = useState(inprocessingItems);
 
   function updateHeader(field, value) {
     const newHeader = { ...header };
@@ -19,6 +21,22 @@ function App() {
     setHeader(newHeader);
   }
 
+  function markAsComplete(id) {
+    const newItems = [...items];
+    let index = newItems.findIndex((item) => item.id === id);
+    if (index > -1) {
+      const completed = newItems[index].completed;
+
+      if (completed) {
+        newItems[index].completed = false;
+      } else {
+        newItems[index].completed = true;
+        newItems[index].completionDate = new Date();
+      }
+    }
+    setItems(newItems);
+  }
+
   return (
     <ThemeProvider theme={theme}>
       <Box sx={{ m: 4 }}>
@@ -28,7 +46,18 @@ function App() {
         Note: You must be in uniform for all your appointments.
       </Typography>
       <Box sx={{ m: 4 }}>
-        <ChecklistContainer />
+        <ChecklistContainer
+          header="Base In-Processing"
+          checklistItems={inprocessingItems}
+          markAsComplete={markAsComplete}
+        />
+      </Box>
+      <Box sx={{ m: 4 }}>
+        <ChecklistContainer
+          header="Base In-Processing"
+          checklistItems={inprocessingItems}
+          markAsComplete={markAsComplete}
+        />
       </Box>
     </ThemeProvider>
   );
--- src/ChecklistContainer.js
+++ src/ChecklistContainer.js
@@ -1,35 +1,27 @@
-import { Box } from "@mui/material";
-import { useState } from "react";
+import { Box, Typography } from "@mui/material";
 import ChecklistItem from "./ChecklistItem";
-import baseInprocessing from "./baseInprocessing.json";
 
-export default function ChecklistContainer() {
-  const [items, setItems] = useState(baseInprocessing);
-
-  function markAsComplete(id) {
-    const newItems = [...items];
-    let index = newItems.findIndex((item) => item.id === id);
-    if (index > -1) {
-      const completed = newItems[index].completed;
-
-      if (completed) {
-        newItems[index].completed = false;
-      } else {
-        newItems[index].completed = true;
-        newItems[index].completionDate = new Date();
-      }
-    }
-    setItems(newItems);
-  }
+export default function ChecklistContainer({
+  header,
+  checklistItems,
+  markAsComplete,
+}) {
   return (
-    <Box className="Checklist">
-      {items.map((item, index) => (
-        <ChecklistItem
-          key={index}
-          item={item}
-          markAsComplete={markAsComplete}
-        />
-      ))}
+    <Box className="csstransforms">
+      <aside>
+        <Typography component="h3" className="Checklist-header">
+          {header}
+        </Typography>
+        <Box className="Checklist">
+          {checklistItems.map((item, index) => (
+            <ChecklistItem
+              key={index}
+              item={item}
+              markAsComplete={markAsComplete}
+            />
+          ))}
+        </Box>
+      </aside>
     </Box>
   );
 }
